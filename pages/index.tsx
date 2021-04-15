7e56cf2f184be8ea2bc264716a755244141bff28
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-15 11:58:03 -0700

    homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,107 @@
+import { Auth } from "@supabase/ui"
 import Head from "next/head"
+import { supabase } from "../lib/supabase"
 import styles from "../styles/Home.module.css"
+
+const fonts = {
+	default: `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+}
+
+const colors = {
+	// Notion ripoff colors:
+	primaryBlue: "#57A8D7",
+}
+
+function Row(props: { children: React.ReactNode }) {
+	return (
+		<div className="row">
+			{props.children}
+			<style jsx>{`
+				.row {
+					margin: 0.5rem 0;
+				}
+			`}</style>
+		</div>
+	)
+}
+
+// See https://github.com/supabase/ui
+function UserLogin(props: {}) {
+	const { user } = Auth.useUser()
+
+	if (user) {
+		return (
+			<>
+				<Row>Signed in: {user.email}</Row>
+				<Row>
+					<button onClick={() => supabase.auth.signOut()}>Sign out</button>
+				</Row>
+				<style jsx>{`
+					button {
+						/* reset */
+						border: none;
+						font-family: ${fonts.default};
+						font-size: 14px;
+
+						/* style */
+						background: ${colors.primaryBlue};
+						box-shadow: inset 0px 0px 0px 1px rgba(0, 0, 0, 0.1);
+						border-radius: 3px;
+						padding: 0.5em 1em;
+						font-weight: bold;
+						color: white;
+					}
+				`}</style>
+			</>
+		)
+	}
+
+	return <Auth supabaseClient={supabase} />
+}
+
+function Emojis(props: {}) {
+	return (
+		<>
+			<span>🥬 🍜 🍲 🍠</span>
+			<style jsx>{`
+				span {
+					font-size: 3em;
+					letter-spacing: -0.3em;
+				}
+			`}</style>
+		</>
+	)
+}
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Notrition - Nutrition & Recipes for Notion</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
+					<Emojis />
 				</h1>
+				<h1 className={styles.title}>Notrition</h1>
 
 				<p className={styles.description}>
-					Get started by editing{" "}
-					<code className={styles.code}>pages/index.js</code>
+					Add nutrition & recipe info to Notion
 				</p>
 
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h3>Documentation &rarr;</h3>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h3>Learn &rarr;</h3>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}
-					>
-						<h3>Examples &rarr;</h3>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h3>Deploy &rarr;</h3>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
+				<UserLogin />
 			</main>
 
 			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
-					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+				<span style={{ margin: "0 0.3em" }}>Made by </span>
+				<a href="https://github.com/vicky11z" target="_blank">
+					Vicky Zhang
+				</a>
+				<span style={{ margin: "0 0.3em" }}> and </span>
+				<a href="https://twitter.com/@jitl" target="_blank">
+					Jake Teton-Landis
 				</a>
 			</footer>
 		</div>
