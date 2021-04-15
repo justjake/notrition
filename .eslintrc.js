d6906d13f2618e09855290d1112511c11e8d34ed
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-14 21:51:50 -0700

    dont lint me bro

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,4 @@ module.exports = {
 	root: true,
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 }
