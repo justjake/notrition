7102fc0f9641be7d148ce2f7e4777f819e035a86
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-14 22:03:03 -0700

    typefix

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiHandler } from "next"
 
-export default (req, res) => {
+const hello: NextApiHandler = (req, res) => {
 	res.status(200).json({ name: "John Doe" })
 }
+
+export default hello
