50a8dba249e15b39ce13ecd8deccc114ade60ee2
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-16 08:42:49 -0700

    fix issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ import { Profile } from "../lib/models"
 import { UserLogin } from "../components/UserSettings"
 import {
 	CreateNotionRecipePage,
-	NotionRecipeExtractor,
 	NotionRecipePageList,
 } from "../components/NotionRecipeExtractor"
 
